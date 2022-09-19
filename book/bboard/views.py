c9b58d12b1e5a1a13de04280fff917a7576d8568
cybersen <gashnya_official@bk.ru>
2022-09-19 22:51:07 +0300

    Ver 2b - Page 43: object bug

--- book/bboard/views.py
+++ book/bboard/views.py
@@ -1,10 +1,6 @@
-from django.http import HttpResponse
-
 from .models import Bb
-
+from django.shortcuts import render
+ 
 def index(request):
-   in_stock_list = 'Щебень оптом \r\n\r\n\r\n Доступное в продаже \n\r\n\r\n'
-   for bb in Bb.objects.order_by('-published'):
-      in_stock_list += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-      
-   return HttpResponse(in_stock_list, content_type= 'text/plain; charset=utf-8')
+    bbs = Bb.objects.all()
+    return render(request, 'bboard/index.html', {'bbs': bbs})
