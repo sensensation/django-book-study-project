f5cb7905dce254a40d76e969d5c1f89f42a8fc70
cybersen <gashnya_official@bk.ru>
2022-09-28 20:54:39 +0300

    Ver 5b - Page 55: Foreign key, new class Rubric. Bug fix view request

--- book/bboard/views.py
+++ book/bboard/views.py
@@ -2,5 +2,6 @@ from .models import Bb
 from django.shortcuts import render
  
 def index(request):
-    bbs = Bb.objects.all()
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
     
