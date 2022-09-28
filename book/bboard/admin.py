2d73d33f15c7db899954566f3f4b212da29a476f
cybersen <gashnya_official@bk.ru>
2022-09-28 20:46:35 +0300

    Ver 5 - Page 55: Foreign key, new class: Rubric

--- book/bboard/admin.py
+++ book/bboard/admin.py
@@ -3,3 +3,7 @@ from django.contrib import admin
 #Регистрация приложения явно в django admin на сайте отладки
 from .models import Bb 
 admin.site.register(Bb)
+
+from .models import Rubric
+admin.site.register(Rubric)
+
--- book/bboard/models.py
+++ book/bboard/models.py
@@ -1,3 +1,4 @@
+from re import S
 from tabnanny import verbose
 from django.db import models
 
@@ -6,8 +7,19 @@ class Bb(models.Model):
    content = models.TextField(null=True, blank=True, verbose_name= 'Описание')
    price = models.FloatField(null=True, blank=True, verbose_name= 'Цена')
    published = models.DateTimeField(auto_now_add = True, db_index= True, verbose_name= 'Опубликовано')
+   
+   rubric = models.ForeignKey('Rubric', null= True, on_delete=models.PROTECT, verbose_name='Рубрика')
 
    class Meta():
       verbose_name_plural = 'Объявления'
       verbose_name = 'Объявление'
       ordering = ['-published']
+
+class Rubric(models.Model):
+   name = models.CharField(max_length=20, verbose_name = 'Название',  db_index= True)
+
+   class Meta():
+      verbose_name_plural = 'Рубрики'
+      verbose_name = 'Рубрика'
+      ordering = ['name']
+
