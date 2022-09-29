cd397a39c60083b4fcef9da7145533afa310bed9
cybersen <gashnya_official@bk.ru>
2022-09-29 11:11:42 +0300

    Ver 5c - Page 55: formated files using BLACK

--- book/bboard/views.py
+++ book/bboard/views.py
@@ -1,7 +1,7 @@
 from .models import Bb
 from django.shortcuts import render
- 
+
+
 def index(request):
-    bbs = Bb.objects.order_by('-published')
-    return render(request, 'bboard/index.html', {'bbs': bbs})
-    
+    bbs = Bb.objects.order_by("-published")
+    return render(request, "bboard/index.html", {"bbs": bbs})
